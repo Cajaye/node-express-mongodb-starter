1ec6fbbad958b40fa464faeee30e40bd9a219aad
Cajaye Clarke <cajayeclarke44@gmail.com>
2022-01-08 00:30:34 -0500

    some error handle

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,10 +2,22 @@ const { CustomAPIError } = require('../errors/index')
 const { StatusCodes } = require('http-status-codes')
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-    if (err instanceof CustomAPIError) {
-        return res.status(err.statusCode).json({ msg: err.message })
+    let customError = {
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER,
+        msg: err.message || 'Something went wrong please try again later'
     }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+    if (err.name === 'ValidationError') {
+        customError.msg = Object.values(err.errors)
+            .map((item) => item.message)
+            .join(',')
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+
+    }
+    if (err.code && err.code === 11000) {
+        customError.msg = `Duplicate for ${Object.keys(err.keyValue)} field`
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+    }
+    return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
